6b80e8946b0925358be3becc78084c5df609e88b
forsythrl <tarrantrl@utexas.edu>
2020-10-20 20:39:12 -0500

    initial outline of readme output

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+## Description
+
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution Guidelines](#contribution-guidelines)
+* [Test Instructions](#test-instructions)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Contribution Guidelines
+
+${contributing}
+
+## Test Instructions
+
+${tests}
+
+## Questions
+
 
 `;
 }
