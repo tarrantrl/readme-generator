bbdb073c7e4d1c63ec0238f36bfda15c0914cd4c
forsythrl <tarrantrl@utexas.edu>
2020-10-20 21:16:58 -0500

    write to file

---
+++ dist/readme.md
@@ -0,0 +1,33 @@
+# Project Title
+  
+## Description
+
+description of project
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution Guidelines](#contribution-guidelines)
+* [Test Instructions](#test-instructions)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+
+install instructions for project
+
+## Usage
+
+usage info for project
+
+## Contribution Guidelines
+
+contribution guidelines for project
+
+## Test Instructions
+
+test instructions for project
+
+## Questions
+
+
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // require inquirer
 const inquirer = require("inquirer");
+// require fs
+const fs = require("fs");
 // require utility function
 const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // array of questions for user
 //the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
@@ -23,99 +26,55 @@ const questions = [
         type: "input",
         name: "description",
         message: "Please enter a description of your project.",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter a description of your project.");
-                return false;
-            }
-        }
     },
     {
         type: "input",
         name: "install",
         message: "Please enter installation instructions for your project.",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter installation instructions for your project.");
-                return false;
-            }
-        }
     },
     {
         type: "input",
         name: "usage",
         message: "Please enter usage information for your project.",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter usage information for your project.");
-                return false;
-            }
-        }
     },
     {
         type: "input",
         name: "contributing",
         message: "Please enter contribution guidelines for your project.",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter contribution guidelines for your project.");
-                return false;
-            }
-        }
     },
     {
         type: "input",
         name: "tests",
         message: "Please enter test instructions for your project.",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter test instructions for your project.");
-                return false;
-            }
-        }
     },
 
     {
         type: "input",
         name: "github",
         message: "Please enter your GitHub username.",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your GitHub username.");
-                return false;
-            }
-        }
     },
     {
         type: "input",
         name: "email",
         message: "Please enter your email.",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter your email.");
-                return false;
-            }
-        }
     },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, data, err => {
+            if (err){
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: "File created!",
+            })
+        })
+    });
 }
 
 // function to initialize program
@@ -123,7 +82,7 @@ function init() {
     return inquirer
     .prompt(questions)
     .then(data => {
-        console.log(generateMarkdown(data));
+        writeToFile('./dist/readme.md', generateMarkdown(data));
     });
 }
 
